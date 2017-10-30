97c002c94a2cb3f7c1e4a1dada275f3c573fbd27
WEPAKdev <pakrstic.pro@gmail.com>
2017-10-30 15:34:41 +0100

    creation de l'interface

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
           color:red;
       }
       #mot{
+          margin-top:15%;
           display:flex;
           justify-content:center;
       }
       #mot .letter{
           font-size:15px;
           border-bottom:2px solid black;
-          width:15px;
-          height:15px;
+          width:40px;
+          height:40px;
           margin-right:10px;
       }
       
@@ -26,32 +27,54 @@
         </div>
     </div>
     <div id="saisie">
+        <input type="text">
+        <button onClick="submit(this.value)">
     </div>
     <div id="compteur">
     </div>
 </body>
-    <script type="text/javascript">
-        var mots = {
-                    "PROFFESSEUR" : ["P","R", "O", "F", "F", "E", "S", "S", "E", "U", "R"],
-                    "COURS" : ["C", "O", "U", "R", "S"],
-                    "ELEVE" : ["E", "L", "E", "V", "E"],
-                    "WEB" : ["W", "E", "B"],
-                    "LOL" : ["L", "O", "L"]
-                };
-                
+    <script type="text/javascript">                
+               
         var motToRandom = ["PROFFESSEUR", "COURS", "ELEVE", "WEB", "LOL"];
+        var chosenWord;
+        var nbMaxChances = 10;
+        var compteur = document.getElementById('compteur');
+        var motDiv = document.getElementById('mot');
+            
+        function initCompteur()
+        {
+            compteur.innerHTML = nbMaxChances;
+        }
         
-        function init(){
-            //def mot
-            var chosenWord = motToRandom[Math.floor(Math.random() * motToRandom.length)];
-            alert(mots["'+ chosenWord +'"][0]);
+        function decCompteur(){
+            compteur.innerHTML -= 1;
+            var nbChances = compteur.innerHTML;            
+            if(nbChances < 0){
+                alert('you lost');
+            }
         }
         
         function placerLettres(){
-            var length = chosenWord.length;
-            for(var i=0; i < length; i++){
-                
+            var length = chosenWord.split('').length;
+            motDiv.innerHTML = '';
+            alert(length);
+            
+            for(var i=0; i < length; i++)
+            {
+                motDiv.innerHTML += "<div class='letter'></div>";
             }
+        }
+        
+        function submit(){
+            alert('test');
+        }
+        
+        function init()
+        {
+            //def mot
+            chosenWord = motToRandom[Math.floor(Math.random() * motToRandom.length)];
+            placerLettres();
+            initCompteur();
         }
         
         init();
